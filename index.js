6fab8e1c88d32bd3cea22c173ae06f5bf1dd1f1c
Wisemar <44879764+Wisemar@users.noreply.github.com>
2018-11-09 18:56:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const prefix = ("/");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setgame("Développeur = Wisemar");
+    bot.user.setgame("Développeur Wisemar");
     console.log("Bot ON");
 });
 
 bot.on('message', msg => {
-    if (msg.content === "Leader"){
-        msg.reply("@✘ Leader-JustR3kt ✘ ")
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
     }
     if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
     }
-    if (msg.content === prefix + "help"){
-        msg.channel.send("1. Leader 2./help 3.")
-        console.log("Une personne a demandé help.")
+    if (msg.content === prefix + "site"){
+        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        console.log("Une personne a demandé pour aller sur ton site.")
     }
 
 });
 
 bot.login("NTEwNDk5ODY0ODgzMzYzODYw.DsdP3Q.rVOYL8Uf3JdZ6khjyfu3i8cGaP0");
+
 
 

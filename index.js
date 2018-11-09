f2349d57e37fd014daf4b5fa720037de45e1cae9
Wisemar <44879764+Wisemar@users.noreply.github.com>
2018-11-09 18:41:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,25 @@ bot.on("ready", function() {
     console.log("Bot ON");
 });
 
+bot.on('message', function (message){
+    if (ban.match(message)){
+        return ban.action(message)
+    }
+});
+
+
+bot.on('message', msg => {
+    if (msg.content === "Leader"){
+        msg.reply("@✘ Leader-JustR3kt ✘ ")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "help"){
+        msg.channel.send("1. Leader 2./help 3.")
+        console.log("Une personne a demandé help.")
+    }
+
+});
+
 bot.login("NTEwNDk5ODY0ODgzMzYzODYw.DsdP3Q.rVOYL8Uf3JdZ6khjyfu3i8cGaP0");

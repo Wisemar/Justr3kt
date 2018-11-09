e57c7e95ed6bddc4823f1dc0ebae3e02c1e72508
Wisemar <44879764+Wisemar@users.noreply.github.com>
2018-11-09 18:59:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,13 @@ const prefix = ("/");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setgame("Développeur Wisemar");
+    bot.user.setgame("En cour de dev, par wisemar");
     console.log("Bot ON");
 });
 
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
-
-});
-
 bot.login("NTEwNDk5ODY0ODgzMzYzODYw.DsdP3Q.rVOYL8Uf3JdZ6khjyfu3i8cGaP0");
+
+
 
 
 
